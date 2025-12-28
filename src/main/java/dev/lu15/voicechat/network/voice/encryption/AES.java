fb07ccbb8d4e46def8f5ae6deeb0635c8d6f7ee6
PeyaPeyaPeyang <peyang@peya.tokyo>
2025-12-28 21:47:15 +0900

    feat(b2.6.0): AES/GCM/NoPadding

--- src/main/java/dev/lu15/voicechat/network/voice/encryption/AES.java
+++ src/main/java/dev/lu15/voicechat/network/voice/encryption/AES.java
@@ -7,6 +7,7 @@ import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.security.InvalidAlgorithmParameterException;
@@ -19,22 +20,25 @@ import java.util.UUID;
 public final class AES {
 
     private static final @NotNull Random RANDOM = new SecureRandom();
-    private static final @NotNull String CIPHER = "AES/CBC/PKCS5Padding";
-    private static final int UUID_LENGTH = 16;
+    private static final @NotNull String CIPHER = "AES/GCM/NoPadding";
+
+    private static final int KEY_LENGTH = 16;
+    private static final int IV_LENGTH = 12;
+    private static final int TAG_LENGTH = 128;
 
     private AES() {
     }
 
     public static byte @NotNull [] getBytesFromUuid(@NotNull UUID uuid) {
-        NetworkBuffer buffer = NetworkBuffer.staticBuffer(UUID_LENGTH);
+        NetworkBuffer buffer = NetworkBuffer.staticBuffer(KEY_LENGTH);
         buffer.write(NetworkBuffer.UUID, uuid);
-        byte[] bytes = new byte[UUID_LENGTH];
-        buffer.copyTo(0, bytes, 0, UUID_LENGTH);
+        byte[] bytes = new byte[KEY_LENGTH];
+        buffer.copyTo(0, bytes, 0, KEY_LENGTH);
         return bytes;
     }
 
     private static byte @NotNull [] generateIv() {
-        byte[] iv = new byte[UUID_LENGTH];
+        byte[] iv = new byte[IV_LENGTH];
         RANDOM.nextBytes(iv);
         return iv;
     }
@@ -44,33 +48,42 @@ public final class AES {
     }
 
     public static byte @NotNull [] encrypt(@NotNull UUID secret,
-                                           byte @NotNull [] data) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
+                                           byte @NotNull [] data)
+            throws NoSuchPaddingException, NoSuchAlgorithmException,
+            InvalidAlgorithmParameterException, InvalidKeyException,
+            IllegalBlockSizeException, BadPaddingException {
+
         byte[] iv = generateIv();
-        IvParameterSpec spec = new IvParameterSpec(iv);
+        GCMParameterSpec spec = new GCMParameterSpec(TAG_LENGTH, iv);
+
         Cipher cipher = Cipher.getInstance(CIPHER);
         cipher.init(Cipher.ENCRYPT_MODE, createKey(secret), spec);
 
         byte[] encrypted = cipher.doFinal(data);
-        byte[] result = new byte[iv.length + encrypted.length];
 
+        byte[] result = new byte[iv.length + encrypted.length];
         System.arraycopy(iv, 0, result, 0, iv.length);
         System.arraycopy(encrypted, 0, result, iv.length, encrypted.length);
         return result;
     }
 
     public static byte @NotNull [] decrypt(@NotNull UUID secret,
-                                           byte @NotNull [] result) throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException {
-        byte[] iv = new byte[UUID_LENGTH];
+                                           byte @NotNull [] result)
+            throws NoSuchPaddingException, NoSuchAlgorithmException,
+            InvalidAlgorithmParameterException, InvalidKeyException,
+            IllegalBlockSizeException, BadPaddingException {
+
+        byte[] iv = new byte[IV_LENGTH];
         System.arraycopy(result, 0, iv, 0, iv.length);
 
         byte[] data = new byte[result.length - iv.length];
         System.arraycopy(result, iv.length, data, 0, data.length);
 
-        IvParameterSpec spec = new IvParameterSpec(iv);
+        GCMParameterSpec spec = new GCMParameterSpec(TAG_LENGTH, iv);
+
         Cipher cipher = Cipher.getInstance(CIPHER);
         cipher.init(Cipher.DECRYPT_MODE, createKey(secret), spec);
 
         return cipher.doFinal(data);
     }
-
 }
